fec80d3f91fc6bc68d39d7ee6548a5f7b106135e
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-06-13 06:22:41 -0400

    9 dynamic directory index created

--- 03_working_with_sinatra/04_preparations/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/04_preparations/book_viewer/book_viewer.rb
@@ -4,6 +4,18 @@ require "sinatra/reloader"
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
   @contents = File.readlines("data/toc.txt")
+  @public_file_local_paths = Dir["public/*"].reject{ |file| File.directory?(file) }
+  @public_files = @public_file_local_paths.map{ |file| File.basename(file) }
+
+  @public_files.reverse! if params['sort']
 
   erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt").split("\n\n").map{ |paragraph| paragraph.gsub("\n", " ") }
+
+  erb :chapter
 end
